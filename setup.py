16e98e8c7f60088d803a3ff67f0f777a40e660ab
Ichsan Nuur Imansyah <48359338+ichsanni@users.noreply.github.com>
2022-12-14 15:08:35 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 
 setup(name='vpngate-l2tp',
-version='0.1',
-description='Create and connect to L2TP connection and use VPNGate servers.',
-url='#',
-author='Ichsan Nuur',
-author_email='ichsan@csar.co.jp',
-license='MIT',
-packages=['vpngate_l2tp'],
-zip_safe=False)
+  version='0.1',
+  description='Create and connect to L2TP connection and use VPNGate servers.',
+  url='https://github.com/ichsanni/vpngate-l2tp',
+  author='Ichsan Nuur',
+  author_email='ichsan@csar.co.jp',
+  license='MIT',
+  packages=['vpngate_l2tp'],
+  zip_safe=False,
+  install_requires=[
+    "requests >= 2.26.0"])
